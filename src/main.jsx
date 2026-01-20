c7b6dab87283f56c744a5f0107a2a8db18966c2b
Ermiyas Werdofa <ermibkl@gmail.com>
2026-01-20 09:47:28 -0500

    Fix case-sensitive ThemeContext import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./Context/ThemeContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
