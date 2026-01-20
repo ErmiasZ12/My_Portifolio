72a111271e3195c5416ef3787e72d79825c7dc06
Ermiyas Werdofa <ermibkl@gmail.com>
2026-01-20 10:27:20 -0500

    Fix ThemeContext import casing in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 
 import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import { ThemeContext } from "../Context/ThemeContext";
 import { FaGithub, FaLinkedin, FaTwitter, FaArrowUp } from "react-icons/fa";
 import styles from "./Footer.module.css";
 
