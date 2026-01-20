03f3dd385b632a3b06d45ad7598405f8e350477a
Ermiyas Werdofa <ermibkl@gmail.com>
2026-01-20 10:06:52 -0500

    Fix components/Projects case for Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// import Hero from "./components/Hero";
+
 import About from "./components/About";
-import Projects from "./components/Projects";
+import Projects from "./components/projects";
 import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
